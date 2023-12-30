c64fa60a649c5609bb360453cdddcb144b5cc8e8
lvlaiwei <lvlaiwei@gmain.com>
2023-12-30 08:06:25 +0000

    add single list data.

---
+++ list/data_structure/single/single_list_data.c
@@ -0,0 +1,123 @@
+#include "single_list_data.h"
+
+void check_pointer(void *pointer)
+{
+    if(!pointer)
+        exit(-1);
+}
+
+list_head *init_head()
+{
+    list_head *head = (list_head *)malloc(sizeof(list_head));
+    check_pointer(head);
+    head->list = NULL;
+    head->total_num = 0;
+
+    return head;
+}
+
+single_list *add_list(int num)
+{
+    single_list *list = (single_list *)malloc(sizeof(single_list));
+    check_pointer(list);
+    list->next = NULL;
+    list->num = num;
+    return list;
+}
+
+void push_list_tail(list_head *head, single_list *list)
+{
+    if(!head->list){
+        head->list = list;
+        return;
+    }
+
+    single_list *temp_list = head->list;
+    while(temp_list->next) temp_list = temp_list->next;
+
+    temp_list->next = list;
+}
+
+void push_list_head(list_head *head, single_list *list)
+{
+    if(!head->list){
+        head->list = list;
+        return;
+    }
+
+    list->next = head->list;
+    head->list = list;
+
+}
+
+void pop_list(list_head *head, int num)
+{
+    if(!head->list){
+        printf("Head is NULL\n");
+        return;
+    }
+    
+    single_list *temp_list = head->list;
+    single_list *pre_list = NULL;
+    while(temp_list->num != num){
+        pre_list = temp_list;
+        temp_list = temp_list->next;
+    }
+
+    pre_list->next = temp_list->next;
+    
+    free(temp_list);
+}
+
+void print_list(list_head *head)
+{
+    if(!head->list){
+        printf("Head is NULL\n");
+        return;
+    }
+
+    single_list *temp_list = head->list;
+    while(temp_list){
+        printf("num is %d\n", temp_list->num);
+        temp_list = temp_list->next;
+    }
+}
+
+void free_all_list(list_head *head)
+{
+    if(!head->list){
+        printf("Head is NULL\n");
+        return;
+    }
+    
+    single_list *temp_list = head->list;
+    single_list *pre_list = NULL;
+    while(temp_list){
+        pre_list = temp_list;
+        temp_list = temp_list->next;
+        free(pre_list);
+    }
+    free(head);
+
+}
+
+int main()
+{
+    list_head *head = init_head();
+    
+    for(int i = 0; i < 9; i++){
+        single_list *list = add_list(i);
+        push_list_tail(head, list);
+    }
+    for(int i = 9; i < 18; i++){
+        single_list *list = add_list(i);
+        push_list_head(head, list);
+    }
+
+    pop_list(head, 6);
+
+    print_list(head);
+
+    free_all_list(head);
+    return 0;
+}
---
+++ list/data_structure/single/single_list_data.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _SINGLE_LIST {
+   struct _SINGLE_LIST *next;
+   int num;
+} single_list;
+
+typedef struct _LIST_HEAD {
+    int total_num;
+    single_list *list;
+} list_head;
+
+void check_pointer(void *pointer);
+list_head *init_head();
+single_list *add_list(int num);
+void push_list_tail(list_head *head, single_list *list);
+void push_list_head(list_head *head, single_list *list);
+void pop_list(list_head *head, int num);
+void print_list(list_head *head);
+void free_all_list(list_head *head);
